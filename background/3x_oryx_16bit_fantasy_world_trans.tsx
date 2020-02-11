283332c117718d74d7f040a080e0a4ec6050f55e
smelc <smelc@users.noreply.github.com>
2020-02-11 11:10:57 +0100

    Commit tiled files, used to assemble the background

---
+++ background/3x_oryx_16bit_fantasy_world_trans.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="3x_oryx_16bit_fantasy_world_trans" tilewidth="72" tileheight="72" tilecount="2296" columns="56">
+ <image source="../oryx/3x_oryx_16bit_fantasy_world_trans.png" width="4098" height="3021"/>
+</tileset>
---
+++ background/archer.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="archer" tilewidth="72" tileheight="72" tilecount="12" columns="3">
+ <image source="../assets-gen/archer.png" width="216" height="288"/>
+</tileset>
