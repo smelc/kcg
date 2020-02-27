e2c0f745fd260f68a0ef583194457af4464b7526
smelc <smelc@users.noreply.github.com>
2020-02-27 19:30:00 +0100

    Format all files with ormolu + make pre-commit hook automatically format modified files

--- ai/src/Lib.hs
+++ ai/src/Lib.hs
@@ -3,8 +3,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc,
+  )
+where
 
 import Data.Aeson
 import Data.ByteString.Lazy
@@ -14,56 +15,72 @@ data Team = Human | Undead
   deriving (Generic, Show)
 
 instance ToJSON Team
+
 instance FromJSON Team
 
-data Skill = 
-    HitFromBack
+data Skill
+  = HitFromBack
   | Ranged
   deriving (Generic, Show)
 
 instance ToJSON Skill
+
 instance FromJSON Skill
 
 creatureOptions :: Options
-creatureOptions = defaultOptions
-  { fieldLabelModifier = impl }
-  where impl "creatureName" = "name"
-        impl "victoryPoints" = "victory_points"
-        impl s = s
+creatureOptions =
+  defaultOptions
+    { fieldLabelModifier = impl
+    }
+  where
+    impl "creatureName" = "name"
+    impl "victoryPoints" = "victory_points"
+    impl s = s
 
-data Creature = Creature
-  { team::Team,
-    creatureName::String,
-    hp::Int,
-    attack::Int,
-    victoryPoints::Int,
-    skills::Maybe [Skill] }
+data Creature
+  = Creature
+      { team :: Team,
+        creatureName :: String,
+        hp :: Int,
+        attack :: Int,
+        victoryPoints :: Int,
+        skills :: Maybe [Skill]
+      }
   deriving (Generic, Show)
 
-data Neutral = Neutral
-  { neutralName::String }
+data Neutral
+  = Neutral
+      {neutralName :: String}
   deriving (Generic, Show)
 
 neutralOptions :: Options
-neutralOptions = defaultOptions
-  { fieldLabelModifier = impl }
-  where impl "neutralName" = "name"
-        impl s = s
+neutralOptions =
+  defaultOptions
+    { fieldLabelModifier = impl
+    }
+  where
+    impl "neutralName" = "name"
+    impl s = s
 
 instance ToJSON Creature
+
 instance FromJSON Creature where
-    parseJSON = genericParseJSON creatureOptions
+  parseJSON = genericParseJSON creatureOptions
 
 instance ToJSON Neutral
-instance FromJSON Neutral where
-    parseJSON = genericParseJSON neutralOptions
 
-data AllData = AllData
-  { creatures::[Creature],
-    neutral::Maybe [Neutral] }
+instance FromJSON Neutral where
+  parseJSON = genericParseJSON neutralOptions
+
+data AllData
+  = AllData
+      { creatures :: [Creature],
+        neutral :: Maybe [Neutral]
+      }
   deriving (Generic, Show)
 
 instance ToJSON AllData
+
 instance FromJSON AllData
 
 someFunc :: IO ()
@@ -73,13 +90,13 @@ someFunc = do
   print decoding
   print decoding2
   print allDecoding
-  where creature :: Creature = Creature Human "archer" 1 1 2 Nothing
-        creatureJSON  = "{ \"team\":\"Human\", \"name\":\"spearman\",  \"hp\":2, \"attack\":1, \"victory_points\":2, \"skills\":[\"HitFromBack\"]}"
-        creature2JSON  = "{ \"team\":\"Human\", \"foobar\":\"ignore\", \"name\":\"spearman\",  \"hp\":2, \"attack\":1, \"victory_points\":2}"
-        decoding :: Either String Creature = eitherDecode creatureJSON
-        decoding2 :: Either String Creature = eitherDecode creature2JSON
-        prefix = "{ \"creatures\": ["
-        suffix = "] }"
-        allDataJSON :: ByteString = append prefix $ append creatureJSON suffix
-        allDecoding :: Either String AllData = eitherDecode allDataJSON
-
+  where
+    creature :: Creature = Creature Human "archer" 1 1 2 Nothing
+    creatureJSON = "{ \"team\":\"Human\", \"name\":\"spearman\",  \"hp\":2, \"attack\":1, \"victory_points\":2, \"skills\":[\"HitFromBack\"]}"
+    creature2JSON = "{ \"team\":\"Human\", \"foobar\":\"ignore\", \"name\":\"spearman\",  \"hp\":2, \"attack\":1, \"victory_points\":2}"
+    decoding :: Either String Creature = eitherDecode creatureJSON
+    decoding2 :: Either String Creature = eitherDecode creature2JSON
+    prefix = "{ \"creatures\": ["
+    suffix = "] }"
+    allDataJSON :: ByteString = append prefix $ append creatureJSON suffix
+    allDecoding :: Either String AllData = eitherDecode allDataJSON
