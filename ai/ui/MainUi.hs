806e0c5919a2c2f59339dabe1a40d9f76921e035
smelc <smelc@users.noreply.github.com>
2020-03-24 18:10:11 +0100

    Set window size to size of background png

--- ai/ui/MainUi.hs
+++ ai/ui/MainUi.hs
@@ -5,21 +5,40 @@ module MainUi where
 import Control.Exception
 import Data.Dynamic
 import Graphics.Gloss
+import Graphics.Gloss.Data.Bitmap
 import Graphics.Gloss.Juicy
 
 backgrounds = ["assets-gen/forest.png"]
 
-data UIException = LoadException FilePath
+data UIException
+  = LoadException FilePath
+  | InternalUnexpectedPictureType FilePath
   deriving (Show, Typeable)
 
 instance Exception UIException
 
+gameName = "Pixel Card Wars" -- Card Combat Retro
+
+pictureSize ::
+  Picture ->
+  Maybe (Int, Int)
+pictureSize picture =
+  case picture of
+    Bitmap bd -> Just $ bitmapSize bd
+    BitmapSection rect _ -> Just $ rectSize rect
+    whatever -> Nothing
+
 mainUI :: IO ()
 mainUI = do
   maybeBG <- loadJuicyPNG bgPath
-  let bg = case maybeBG of
-        Nothing -> throw $ LoadException bgPath
-        Just (value :: Picture) -> value
-  display (InWindow "Nice Window" (640, 640) (0, 0)) white bg
+  let bg =
+        case maybeBG of
+          Nothing -> throw $ LoadException bgPath
+          Just (value :: Picture) -> value
+  let bgSize =
+        case pictureSize bg of
+          Nothing -> throw $ InternalUnexpectedPictureType bgPath
+          Just size -> size
+  display (InWindow gameName bgSize (0, 0)) white bg
   where
     bgPath :: FilePath = backgrounds !! 0
